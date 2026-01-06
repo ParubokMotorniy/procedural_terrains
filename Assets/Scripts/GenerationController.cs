b130a43593bb0d1e85a3bfd91690fd68d9fb2c93
kaba4ok <oles.yarish@gmail.com>
2026-01-06 18:32:17 +0200

    Add basic rendering of compute-filled color texture

---
+++ Assets/Scripts/GenerationController.cs
@@ -0,0 +1,43 @@
+using Unity.Mathematics;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+[RequireComponent(typeof(Renderer))]
+public class GenerationControllerr : MonoBehaviour
+{
+
+    [SerializeField]
+    private ComputeShader shaderToDispatch;
+
+    [Range(1, 64)]
+    public int dispatchDimension;
+
+    private RenderTexture noiseRenderTexture;
+
+    void Start()
+    {
+        noiseRenderTexture = new RenderTexture(dispatchDimension, dispatchDimension, 0)
+        {
+            graphicsFormat = UnityEngine.Experimental.Rendering.GraphicsFormat.R8G8B8A8_UNorm,
+            useMipMap = false,
+            enableRandomWrite = true,
+            filterMode = FilterMode.Bilinear,
+            wrapMode = TextureWrapMode.Clamp
+        };
+        noiseRenderTexture.Create();
+
+        Assert.IsTrue(noiseRenderTexture.IsCreated());
+
+        int kernelIdx = shaderToDispatch.FindKernel("UberNoiseTerrainGenerator");
+        shaderToDispatch.SetTexture(kernelIdx, Shader.PropertyToID("Result"), noiseRenderTexture);
+        shaderToDispatch.SetInt("dispatchDimension", dispatchDimension);
+        shaderToDispatch.Dispatch(kernelIdx, dispatchDimension, dispatchDimension, 1);
+
+        GetComponent<Renderer>().material.mainTexture = noiseRenderTexture;
+    }
+
+    void Update()
+    {
+
+    }
+}
